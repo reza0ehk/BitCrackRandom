6295d21da572a19ba39c937bbbe8595bda43ca51
Mohamamd Eshbeata <m.eshbeata@gmail.com>
2024-03-03 17:39:27 +0300

    fix iinput

--- KeyFinder/main.cpp
+++ KeyFinder/main.cpp
@@ -620,11 +620,6 @@ int main(int argc, char **argv)
 		}
 	}
 
-    if (optContinue && _config.randomMode) {
-        Logger::log(LogLevel::Error, "Random and continue mode cannot be used together.");
-        return 1;
-    }
-
     if(listDevices) {
         printDeviceList(_devices);
         return 0;
