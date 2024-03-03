32983976049d48df64178c6c0692be92c4b5088c
Mohamamd Eshbeata <m.eshbeata@gmail.com>
2024-03-03 17:28:10 +0300

    fixing missing functions

--- secp256k1lib/secp256k1.h
+++ secp256k1lib/secp256k1.h
@@ -255,6 +255,19 @@ namespace secp256k1 {
 
 			return (this->v[n / 32] & (0x1 << (n % 32))) != 0;
 		}
+		
+		int getBitRange()
+		{
+			int ret = 0;
+			for(int n = 255; n >= 0; n--) {
+				if(bit(n)) {
+					ret = n + 1;
+					break;
+				}
+			}
+
+			return ret;
+		}
 
 		bool isEven()
 		{
@@ -268,6 +281,9 @@ namespace secp256k1 {
             return ((uint64_t)this->v[1] << 32) | v[0];
         }
 	};
+
+	static Random rnd;
+	uint256 getRandomRange(uint256 min, uint256 max);
 
 	const unsigned int _POINT_AT_INFINITY_WORDS[8] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 	const unsigned int _P_WORDS[8] = { 0xFFFFFC2F, 0xFFFFFFFE, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
